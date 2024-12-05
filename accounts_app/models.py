7fe70d35fcc1780ba9d3d12a32b0b94239a42db9
Lervez5 <enockbosire756@gmail.com>
2024-12-05 09:29:02 +0300

    Few changes

--- accounts_app/models.py
+++ accounts_app/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    User = models.OneToOneField(User, on_delete=models.CASCADE)
     phone = models.CharField(max_length=15)
     profile_picture = models.ImageField(upload_to='profile_pics/',blank=True,null=True)
     location = models.CharField(max_length=100)
--- proj_licious/settings.py
+++ proj_licious/settings.py
@@ -85,21 +85,22 @@ DATABASES = {
 }
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-    
-    # {
+    'default': 
+    {
         
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': '',
-    #     'USER' : 'root',
-    #     'PASSWORD' : '',
-    #     'HOST' : '127.0.0.1',
-    #     'PORT' : '3306',
-    # }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dinereserve',
+        'USER' : 'root',
+        'PASSWORD' : '',
+        'HOST' : '127.0.0.1',
+        'PORT' : '3306',
+    }
 }
+    # {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+    
 
 
 # Password validation
